19b47af02840585b5c9e60e4497bb176b3fe22c3
SessionHu <sessionhu@outlook.com>
2026-03-26 20:12:24 +0800

    fix: dig -f and -k read files

--- src/dig.d
+++ src/dig.d
@@ -1,9 +1,15 @@
 import std.array : array;
 import std.process : execv;
-import std.regex : ctRegex, splitter;
+import std.regex : ctRegex, match, splitter;
 import std.stdio : stdout;
 
 void dig(string list) {
+  if (list.match(ctRegex!r"^(.+\s)?-(f|k).*$")) {
+    stdout.write("Status: 403 Forbidden\r\n");
+    stdout.write("content-type: text/plain\r\n\r\n");
+    stdout.writeln("-f and -k not allowed here");
+    return;
+  }
   stdout.write("content-type: text/plain\r\n");
   stdout.write("\r\n");
   stdout.flush();
