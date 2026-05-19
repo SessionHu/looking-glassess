ce28094579988574911d119d9bd6cc54a267c013
SessionHu <sessionhu@outlook.com>
2026-05-20 07:46:24 +0800

    feat: add bsd support

--- src/kroute.d
+++ src/kroute.d
@@ -1,13 +1,21 @@
 import std.process : execv;
 import std.stdio : stdout;
 
+
 void kroute(string dst, string ipv) {
-  auto cmd = ["ip"];
-  if (ipv == "4" || ipv == "6") cmd ~= ["-" ~ ipv];
-  cmd ~= ["route"];
-  if (dst.length > 0) cmd ~= ["get", dst];
   stdout.write("content-type: text/plain\r\n");
   stdout.write("\r\n");
   stdout.flush();
-  execv("/usr/bin/ip", cmd);
+  version (linux) version (Android) {
+    auto cmd = ["ip"];
+    if (ipv == "4" || ipv == "6") cmd ~= ["-" ~ ipv];
+    cmd ~= ["route"];
+    if (dst.length > 0) cmd ~= ["get", dst];
+    execv("/usr/bin/ip", cmd);
+  } else {
+    auto cmd = ["route"];
+    if (ipv == "4" || ipv == "6") cmd ~= ["-" ~ ipv];
+    if (dst.length > 0) cmd ~= ["get", dst];
+    execv("/sbin/route", cmd);
+  }
 }
