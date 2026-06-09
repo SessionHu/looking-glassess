c6adaf8aee096e50b6a709195b39bda74dbd4eca
SessionHu <sessionhu@outlook.com>
2026-06-09 09:16:09 +0800

    fix(trace): timeout and ipv6 on freebsd

--- src/trace.d
+++ src/trace.d
@@ -1,18 +1,24 @@
 import std.process : execv;
 import std.stdio : stdout;
+import std.string : indexOf;
 import std.system : OS, os;
 
 void trace(string hst, string ipv, string proto) {
-  auto cmd = ["traceroute"];
-  if (ipv == "4" || ipv == "6") cmd ~= ["-" ~ ipv];
+  string[] cmd = [];
+  static if (os == OS.android) {
+    cmd ~= ["/data/data/com.termux/files/usr/bin/traceroute"];
+  } else {
+    cmd ~= ["/usr/sbin/traceroute"];
+  }
+  static if (os == OS.freeBSD) {
+    if (ipv == "6" || indexOf(hst, ":") > -1) cmd[0] ~= "6";
+  } else {
+    if (ipv == "4" || ipv == "6") cmd ~= ["-" ~ ipv];
+  }
   if (proto == "icmp") cmd ~= "-I";
   cmd ~= ["--", hst];
   stdout.write("content-type: text/plain\r\n");
   stdout.write("\r\n");
   stdout.flush();
-  static if (os == OS.android) {
-    execv("/data/data/com.termux/files/usr/bin/traceroute", cmd);
-  } else {
-    execv("/usr/sbin/traceroute", cmd);
-  }
+  execv("/bin/timeout", ["timeout", "-k10", "-sINT", "60s"] ~ cmd);
 }
